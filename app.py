b49ce6cad7422bb95b8193d3fe8b4ee8f9367778
Ionut204 <iordachi.phone@gmail.com>
2026-03-03 21:21:12 +0200

    implement Firebase email/password authentication

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
-import streamlit.components.v1 as components
-from pathlib import Path
-
+import pyrebase
 st.set_page_config(page_title="Portofolio Tracker",layout="wide")
 
+firebaseConfig = dict(st.secrets["firebase"])
+
+firebase= pyrebase.initialize_app(firebaseConfig)
+auth=firebase.auth()
+
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in= False
 def login_screen():
     col1, col2, col3 = st.columns([1, 1, 1])
 
@@ -15,15 +20,29 @@ def login_screen():
             password = st.text_input("Password", type="password")
 
             submit_button = st.form_submit_button("Submit",width="stretch")
+
             if submit_button:
-                st.write(f"The system is attempting to log in for: {email}")
+                try:
+                    user= auth.sign_in_with_email_and_password(email,password)
+
+                    st.session_state.logged_in=True
+                    st.session_state.user_email=email
+                    st.rerun()
+                    st.success("Log in successful.")
+                except Exception as e:
+                    st.error("Email or password is wrong.")
 
 
-if not st.user.is_logged_in:
+if not st.session_state.logged_in and not st.user.is_logged_in:
     login_screen()
     col1,col2,col3= st.columns([1,1,1])
     with col2:
         st.button("Log in with Google", on_click=st.login, use_container_width=True)
 else:
-    st.header(f"Welcome, {st.user.name}!")
-    st.button("Log out", on_click=st.logout)
+    display_name = st.session_state.get("user_email") or st.user.name
+    st.header(f"Welcome, {display_name}!")
+    if st.button("Log out"):
+        st.session_state.logged_in = False
+        if st.user.is_logged_in:
+            st.logout()
+        st.rerun()
